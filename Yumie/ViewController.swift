da83c4b16c631ddb349174c4e9b28dbe068de464
Alex <alexmihgel@gmail.com>
2022-09-04 13:12:49 +0300

    Initial Commit

---
+++ Yumie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yumie
+//
+//  Created by Алексей Попроцкий on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
