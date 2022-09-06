6828071742bf0cd8de7412dc559a8426f5beba2b
Alex <alexmihgel@gmail.com>
2022-09-06 15:12:37 +0300

    add pod Kingfisher

---
+++ Yumie/CustomViews/ColletcionViewCells/CategoryColletionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Yumie
+//
+//  Created by Алексей Попроцкий on 06.09.2022.
+//
+
+import UIKit
+import Kingfisher
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+    }
+}
---
+++ Yumie/Models/DishCategory.swift
@@ -0,0 +1,15 @@
+//
+//  DishCategory.swift
+//  Yumie
+//
+//  Created by Алексей Попроцкий on 06.09.2022.
+//
+
+import Foundation
+
+
+struct DishCategory {
+    let id: String
+    let name: String
+    let image: String
+}
